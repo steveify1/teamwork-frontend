56e68985ee744213c34aebf2f1194b215bd70809
Nwakasi Stephen Ifeanyi <nwakasistephen@gmail.com>
2019-11-29 23:53:11 +0100

    Updates

---
+++ precache-manifest.24ecd1b84c92c9974f53cc4520ca5c51.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96568e3644d7145c47d2f4d5dad5b4ea",
+    "url": "/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "39054bbef0ddbe5dc831",
+    "url": "/teamwork-frontend/static/css/main.19957c7f.chunk.css"
+  },
+  {
+    "revision": "7c50a194c021edfeedd2",
+    "url": "/teamwork-frontend/static/js/2.1543a0dc.chunk.js"
+  },
+  {
+    "revision": "39054bbef0ddbe5dc831",
+    "url": "/teamwork-frontend/static/js/main.820b9db9.chunk.js"
+  },
+  {
+    "revision": "c72af4ddc98ec8359cab",
+    "url": "/teamwork-frontend/static/js/runtime-main.b288330e.js"
+  },
+  {
+    "revision": "ba8fda24ce4a06757e415cb5b108a039",
+    "url": "/teamwork-frontend/static/media/bg4-2.ba8fda24.svg"
+  },
+  {
+    "revision": "9cd2e14aeb113755b011aea80019ca2e",
+    "url": "/teamwork-frontend/static/media/red-paper-airplane-fly-dark-blue-background_44481-304.9cd2e14a.jpg"
+  },
+  {
+    "revision": "90aaa58f6d569956804876458de8a611",
+    "url": "/teamwork-frontend/static/media/standing-20.90aaa58f.svg"
+  }
+]);
